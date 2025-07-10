2a43232cb0cfb1e645b515ecde31f834e8fb9456
Rafafi35 <salinger.rafael@gmail.com>
2025-07-10 11:02:40 +0200

    implement stop

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ let startTime
 let elapsedTime = 0
 let isRunning = false
 
+function handleClick(){
+    if(!isRunning){
+        start()
+    } else {
+        stop()
+    }
+}
+
 
 function start(){
     if(!isRunning){
-        startTime = Date.now() - elapsedTime
+        startTime = Date.now()
         timer = setInterval(update, 1)
         isRunning = true
     }
 }
 
 function stop(){
-
+    if(isRunning){
+        clearInterval(timer)
+        elapsedTime = Date.now() - startTime
+        isRunning = false
+    }
 }
 
 function update(){
     const currentTime = Date.now()
     elapsedTime = currentTime - startTime
-
     display.textContent = elapsedTime
 }
