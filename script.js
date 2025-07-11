bb57e4a4ff7f3214330e5ec2d56b174752f7297b
Rafafi35 <salinger.rafael@gmail.com>
2025-07-11 15:57:11 +0200

    reformat

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ let historyLength = 0
 let historySum = 0
 let averageTime = 0
 
-function reactionClick(){
-    if(mode === 1){
+function reactionClick() {
+    if (mode === 1) {
         start()
     } else {
         stop()
@@ -21,8 +21,8 @@ function reactionClick(){
 }
 
 
-function start(){
-    if(mode === 1){
+function start() {
+    if (mode === 1) {
         stopAfterTime = Math.random() * 1500 + 2000
         startTime = Date.now()
         timer = setInterval(update, 1)
@@ -32,30 +32,30 @@ function start(){
     }
 }
 
-function stop(){
-        clearInterval(timer)
-        if (mode === 2) {
-            displayReaction.textContent = "You Pressed To early"
-            button.style.backgroundColor = "orange"
-        } else if (mode === 3) {
-            const currentTime2 = Date.now()
-            reactionTime = currentTime2 - startTime
-            displayReaction.textContent = reactionTime
+function stop() {
+    clearInterval(timer)
+    if (mode === 2) {
+        displayReaction.textContent = "You Pressed To early"
+        button.style.backgroundColor = "orange"
+    } else if (mode === 3) {
+        const currentTime2 = Date.now()
+        reactionTime = currentTime2 - startTime
+        displayReaction.textContent = reactionTime
 
-            historyLength += 1
-            historySum += reactionTime
-            averageTime = Math.round(historySum / historyLength)
-            avgText.textContent = "Your average reaction time is " + averageTime + "ms"
+        historyLength += 1
+        historySum += reactionTime
+        averageTime = Math.round(historySum / historyLength)
+        avgText.textContent = "Your average reaction time is " + averageTime + "ms"
 
-        }
-        mode = 1
+    }
+    mode = 1
 }
 
-function update(){
+function update() {
     const currentTime = Date.now()
     elapsedTime = currentTime - startTime
 
-    if (elapsedTime >= stopAfterTime){
+    if (elapsedTime >= stopAfterTime) {
         displayReaction.textContent = "Press!"
         mode = 3
         button.style.backgroundColor = "#0dd10d"
@@ -73,11 +73,11 @@ let firstClick = true
 let highestClicks = 0
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function cpsClick() {
-    if (firstClick){
+    if (firstClick) {
         firstClick = false
         updateCps()
     }
@@ -103,30 +103,33 @@ let reihenfolge = []
 let step = 0
 const grid = document.getElementById("grid")
 
-async function buttonClick(buttonIndex){
+async function buttonClick(buttonIndex) {
 
-        if (reihenfolge[step] === buttonIndex){
-            console.log("richtig " + reihenfolge[step])
-            step++
-            if (step === reihenfolge.length) {
-                console.log("Reihenfolge geschaft")
-                await showPattern()
-                step = 0
 
-            }
 
-        } else {
-            console.log("flasch " + reihenfolge[step] + " = " + buttonIndex)
+    if (reihenfolge[step] === buttonIndex) {
+        console.log("richtig " + reihenfolge[step])
+        step++
+        if (step === reihenfolge.length) {
+            console.log("Reihenfolge geschaft")
+            await showPattern()
             step = 0
-            reihenfolge = []
+            score += 1
+
         }
+
+    } else {
+        console.log("flasch " + reihenfolge[step] + " = " + buttonIndex)
+        step = 0
+        reihenfolge = []
     }
+}
 
 async function showPattern() {
 
     await sleep(600)
 
-    let boxIndex = Math.floor(Math.random() * 9 ) + 1
+    let boxIndex = Math.floor(Math.random() * 9) + 1
     reihenfolge.push(boxIndex)
     for (let i = 0; i < reihenfolge.length; i++) {
         let box = grid.querySelector(`:nth-child(${reihenfolge[i]})`)
